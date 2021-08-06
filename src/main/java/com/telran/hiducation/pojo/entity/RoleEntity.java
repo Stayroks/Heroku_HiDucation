b737097b6e370083976dc46b6580a5606f943a8c
Stayroks <stayroks@gmail.com>
2021-08-06 10:56:08 +0300

    change Identity -> auto

--- src/main/java/com/telran/hiducation/pojo/entity/RoleEntity.java
+++ src/main/java/com/telran/hiducation/pojo/entity/RoleEntity.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class RoleEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String name;
